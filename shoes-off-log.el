17423b80eaf4eb62aefc645ccd2d354902282a7d
Nic Ferier <nic@ferrier.me.uk>
2013-04-20 11:19:41 +0100

    fixups for huskie integration

--- shoes-off-log.el
+++ shoes-off-log.el
@@ -32,31 +32,35 @@ If nil, logs aren't kept."
      shoes-off-log-name
      (concat 
       "while read line;"
-      "do echo $line >> %s-$(date \"+%%Y%%m%%d%%H\");"
+      "do echo $line > %s-$(date \"+%%Y%%m%%d%%H\");"
       "done"))
     (huskie-bind-logname->filename
      shoes-off-log-name
      (format "%s/my-log" (expand-file-name shoes-off-log-dir)))
     (add-hook 'rcirc-print-hooks 'shoes-off-write-log-hook)))
 
+(defun shoes-off/hook-test (process)
+  (and shoes-off-log-dir)
+  (process-get process :shoes-off))
+
 (defun shoes-off-write-log-hook (process sender response target text)
   ;; we should check that process is a shoes-off process
   ;; and not an ordinary rcirc process
-  (when (and shoes-off-log-dir (process-get process :shoes-off))
-    (with-temp-buffer
-      ;; Sometimes TARGET is a buffer :-(
-      (when (bufferp target)
-        (setq target (with-current-buffer buffer rcirc-target)))
-      ;; Sometimes buffer is not anything at all!
-      (unless (or (null target) (string= target ""))
-        (shoes-off-log
-         ;; Print the line into the temp buffer.
-         (concat 
-          (format-time-string "%Y-%m-%d@%H:%M ")
-          (format "{%-16s} " (rcirc-user-nick sender))
-          (unless (string= response "PRIVMSG")
-            (concat "/" (downcase response) " "))
-          text "\n"))))))
+  (when (shoes-off/hook-test process)
+    (when (bufferp target)
+      (setq target (with-current-buffer buffer rcirc-target)))
+    ;; Sometimes buffer is not anything at all!
+    (unless (or (null target) (string= target ""))
+      (shoes-off-log
+       ;; Print the line into the logger
+       (concat
+        ;; Should we send some indication of the target?
+        (format-time-string "%Y-%m-%d@%H:%M ")
+        (format "{%s} {%s} "
+                (rcirc-user-nick sender) target)
+        (unless (string= response "PRIVMSG")
+          (concat "/" (downcase response) " "))
+        text "\n")))))
 
 (provide 'shoes-off-log)
 
